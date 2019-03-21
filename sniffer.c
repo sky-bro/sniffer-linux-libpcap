d3bca50a4e161454b1051ad0852c21fa5e0299e4
sky <sky_bro@qq.com>
2019-03-21 16:57:53 +0800

    test monitor mode, filter out TCP

--- sniffer.c
+++ sniffer.c
@@ -15,7 +15,8 @@ int main(int argc, char *argv[])
   char errbuf[PCAP_ERRBUF_SIZE];	/* Error string */ // 存放错误时的错误提示
   struct bpf_program fp;		/* The compiled filter */ // 存放编译后的规则
   // char filter_exp[] = "port 23";	/* The filter expression */ // 规则字符串
-  char filter_exp[] = "dst host 219.217.228.102"; // 目的ip为教务处网站 jwts.hit.edu.cn
+  // char filter_exp[] = "dst host 219.217.228.102"; // 目的ip为教务处网站 jwts.hit.edu.cn
+  char filter_exp[] = "tcp";
   bpf_u_int32 mask;		/* Our netmask */ // 掩码
   bpf_u_int32 net;		/* Our IP */ // 网络地址部分
   struct pcap_pkthdr header;	/* The header that pcap gives us */
@@ -37,6 +38,7 @@ int main(int argc, char *argv[])
   // }
 
   // 创建handle
+  dev = "wlp3s0";
   handle = pcap_create(dev, errbuf);
   if (handle == NULL) {
     fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
@@ -105,30 +107,52 @@ void callback(u_char* user,const struct pcap_pkthdr* header,const u_char* pkt_da
     // cout<<"\t\t抓到一个包"<<endl;
     // cout<<"-------------------------------------------------"<<endl;
     //解析数据包IP头部
-    if(header->len>=14){
-        pkt_data += 14; // IP头开始
-        IPHeader_t *ip_header=(IPHeader_t*)(pkt_data);
+    // printf("-------------------ORIGINAL BEGIN------------------------\n");
+    // for (int i = 0; i < 15; i++){
+    //   for (int j = 0; j < 8; j++) {
+    //     printf("%02x ", *(pkt_data+i*8+j));
+    //   }
+    //   printf("\n");
+    // }
+    // // printf("first four bytes: %c%c%c%c\n", *pkt_data, *(pkt_data+1), *(pkt_data+2), *(pkt_data+3));
+    // printf("\n-------------------ORIGINAL FINISH------------------------\n");
+    // return;
+    ieee80211_radiotap_header *radio_header = (ieee80211_radiotap_header*)(pkt_data);
+    int radio_header_len = /*ntohs*/(radio_header->len);
+    printf("radio_header_len: %d\n", radio_header_len);
+    pkt_data += radio_header_len;//+34; // IP头开始
+    // printf("first byte: %0x--%0x\n", *(pkt_data), (*pkt_data)&0xc);
+    if (((*pkt_data)&0xc) == 8) { //is data frame
+      // printf("********IAMIN*****");
+      // pkt_data += 26; // LLC开始
+      pkt_data += 34; // IP开始
+      // printf("radio_header_len: %d\n", radio_header_len);
+      IPHeader_t *ip_header=(IPHeader_t*)(pkt_data);
 
-        if (ip_header->Protocol == 6){
-          int ip_total_len = ntohs(ip_header->TotalLen);
-          int ip_header_len = ((ip_header->Ver_HLen)&0xf)*4;
+      if (ip_header->Protocol == 6){
+        int ip_total_len = ntohs(ip_header->TotalLen);
+        int ip_header_len = ((ip_header->Ver_HLen)&0xf)*4;
 
-          pkt_data += ip_header_len; // TCP头开始
-          TCPHeader_t *tcp_header=(TCPHeader_t*)(pkt_data);
-          int tcp_header_len = tcp_header->HeaderLen >> 2;
-          int tcp_content_len = ip_total_len-ip_header_len-tcp_header_len;
-          printf("got a TCP packet, ip_total_len: %d, ip_header_len: %d, tcp_header_len: %d, content_len: %d\n", ip_total_len, ip_header_len, tcp_header_len, tcp_content_len);
+        pkt_data += ip_header_len; // TCP头开始
+        TCPHeader_t *tcp_header=(TCPHeader_t*)(pkt_data);
+        int tcp_header_len = tcp_header->HeaderLen >> 2;
+        int tcp_content_len = ip_total_len-ip_header_len-tcp_header_len;
+        printf("got a TCP packet, ip_total_len: %d, ip_header_len: %d, tcp_header_len: %d, content_len: %d\n", ip_total_len, ip_header_len, tcp_header_len, tcp_content_len);
 
-          // 读取TCP内容
-          pkt_data += tcp_header_len;
-          if (strncmp(pkt_data, "POST", 4) == 0){
-            printf("-------------------POST BEGIN------------------------\n");
-            for (int i = 0; i < tcp_content_len; i++){
-              printf("%c", *(pkt_data+i));
-            }
-            // printf("first four bytes: %c%c%c%c\n", *pkt_data, *(pkt_data+1), *(pkt_data+2), *(pkt_data+3));
-            printf("\n-------------------POST FINISH------------------------\n");
+        // 读取TCP内容
+        pkt_data += tcp_header_len;
+        if (strncmp(pkt_data, "POST", 4) == 0){
+          printf("-------------------POST BEGIN------------------------\n");
+          for (int i = 0; i < tcp_content_len; i++){
+            printf("%c", *(pkt_data+i));
           }
+          // printf("first four bytes: %c%c%c%c\n", *pkt_data, *(pkt_data+1), *(pkt_data+2), *(pkt_data+3));
+          printf("\n-------------------POST FINISH------------------------\n");
         }
+      }
     }
+
+    // if(header->len>=14){
+    //
+    // }
 }
